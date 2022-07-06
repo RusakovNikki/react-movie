a6c54164012a3d4ba3aa800e6b7eab0b0e55f357
ivanov-in <work.ivanov.in@gmail.com>
2022-07-07 00:22:03 +0300

    add routes, header and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainPage from "./pages/MainPage";
+
 function App() {
   return (
-    <div>
-      ВИКА ПУПУСЬКА
-    </div>
+    <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={ <MainPage/> } />
+    </Routes>
+    <Footer/>
+    </>
   );
 }
 
