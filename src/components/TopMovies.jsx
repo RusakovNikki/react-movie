1257f2b8905a1d943f9ca21b56d8796f4cfad450
Yuliya Kiryushina <yulya.ivleva75@gmail.com>
2022-07-11 14:35:19 +0300

    removed setTimeout from a hook, added api keys in comments

--- src/components/TopMovies.jsx
+++ src/components/TopMovies.jsx
@@ -17,7 +17,7 @@ class TopMovies extends React.Component {
     fetch(URL, {
       method: 'GET',
       headers: {
-        'X-API-KEY': 'cc5bbf2a-79b9-4de6-a091-234be04f22a8',
+        'X-API-KEY': '6189fc94-f92f-49e4-add4-368fbca3c2e0', //6189fc94-f92f-49e4-add4-368fbca3c2e0  cc5bbf2a-79b9-4de6-a091-234be04f22a8
         'Content-Type': 'application/json',
       },
     })
--- src/hooks/useGetDescription.js
+++ src/hooks/useGetDescription.js
@@ -8,15 +8,15 @@ export function useGetDescription(id) {
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = setTimeout(await fetch(`${API_DESC_URL}${id}`, {
+            const response = await fetch(`${API_DESC_URL}${id}`, {
                 method: 'GET',
                 headers: {
-                    'X-API-KEY': 'cc5bbf2a-79b9-4de6-a091-234be04f22a8',
+                    'X-API-KEY': '6189fc94-f92f-49e4-add4-368fbca3c2e0', //6189fc94-f92f-49e4-add4-368fbca3c2e0  cc5bbf2a-79b9-4de6-a091-234be04f22a8
                     'Content-Type': 'application/json',
                 }
             })
                 .then(res => res.json())
-                .catch(err => console.log(err)), 1000)
+                .catch(err => console.log(err))
             setItems(response);
         }
         fetchData();
