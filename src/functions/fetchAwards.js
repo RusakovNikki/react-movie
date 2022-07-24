5ce29f8bbab2a4d797d1d40c20af36bade139997
Yuliya Kiryushina <yulya.ivleva75@gmail.com>
2022-07-24 17:24:33 +0300

    Добавила комментарии к функциям, удалила ненужную функцию

--- src/functions/fetchAwards.js
+++ src/functions/fetchAwards.js
@@ -1,6 +1,8 @@
 import { getAwards } from "../utils/api"
 import { getOrderedArray } from "./getOrderedArray";
 
+/*Функция получает только те награды, которые фильм выиграл*/
+
 export const fetchAwards = async (id) => {
     const allAwards = await getAwards(id);
     let winAwards = allAwards.items.filter(e => {
--- src/functions/fetchFacts.js
+++ src/functions/fetchFacts.js
@@ -1,6 +1,8 @@
 import { getFacts } from "../utils/api"
 import { cutOutTags } from "./cutOutTags";
 
+/*Функция получает только 5 фактов (API возвращает по 25+)*/
+
 export const fetchFacts = async (id) => {
     let counter = 1;
     const facts = await getFacts(id);
